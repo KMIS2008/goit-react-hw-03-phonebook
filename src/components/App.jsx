2dff7032fdf04fdddbaec7897245758960d92488
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-11-08 17:32:37 +0200

    prevHomeWork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,85 @@
-// import { Component } from 'react';
+// import { render } from '@testing-library/react';
+import { Component } from 'react';
+import { nanoid } from 'nanoid'
 import { GlobalStyle } from './GlobalStyle';
+import {FormAddContact} from './FormAddContact/FormAddContact';
+import {SectionTitle} from './SectionTitle/SectionTitle';
+import {ContactsList} from './Contacts/Contacts';
+import {FilterConctacts} from './Filter/Filter';
+import {TitleContacts,
+        Contater} from './App.styled';
 
-export const App = () => {
+
+export class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  }
+
+  addContact =(newContact)=> {
+
+    const newAddContact = this.state.contacts.some(contact=> newContact.name.toLowerCase() === contact.name.toLowerCase());
+
+      if(newAddContact){
+        alert(`${newContact.name} is already in contacts`);
+        return;
+      }
+
+     this.setState(prevState=> {
+     return {
+     contacts: [...prevState.contacts,
+                {
+                  ...newContact,
+                  id: nanoid(),
+                }
+              ],
+          };
+       });
+  }
+
+  filterName =(newName)=>{
+    this.setState(
+     {filter: newName} 
+    )
+  }
+
+  deleteContact = contactId=>{
+     this.setState(prevState=>{
+     return {
+      contacts: prevState.contacts.filter(contact=> contact.id !== contactId),
+     };
+    });
+  };
+
+  render(){
+
+    const visibleContact = this.state.contacts.filter(contact=>{
+      const hasContact = contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
+      return hasContact;
+    })
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <Contater>
+     
+    <SectionTitle title="Phonebook"/>
+
+    <FormAddContact onAdd={this.addContact} />
+   
+    <TitleContacts>Contacts</TitleContacts>
+
+    <FilterConctacts filter={this.state.filter} onNameFilter={this.filterName}/>
+    
+    <ContactsList contacts ={visibleContact} ondelete={this.deleteContact}/>
+  
+
       <GlobalStyle/>
-    </div>
+
+    </Contater>
   );
+  }
 };
